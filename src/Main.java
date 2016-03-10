6e2b198d5e462930075215a8f7f02d07c065e140
Raymond Seger <raymondseger@live.com>
2016-03-10 23:24:54 +0700

    Just Google it!

--- src/Main.java
+++ src/Main.java
@@ -6,44 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Product door        = new Product("Wooden Door", 35);
-        Product floorPanel  = new Product("Floor Panel", 25);
-        Product window      = new Product("Glass Window", 10);
-
-        // This collection will only have Product instances
-        Collection<Product> products = new ArrayList<>();
-        products.add(door);
-        products.add(floorPanel);
-        products.add(window);
-
-        // System.out.println(products);
-
-        // test Iterator inside a collection
-        // Way 1
-        final Iterator<Product> productIterator = products.iterator();
-        while(productIterator.hasNext()) {
-            Product product = productIterator.next();
-            if (product.getWeight() > 20) {
-                System.out.println(product);
-            } else {
-                // You need to use the iterator because the object is being iterated, if you modify it, it will give error
-                productIterator.remove();
-            }
-        }
-
-        System.out.println(products);
-
-        // Way 2, same thing with Way 1
-        for( Product product: products) {
-            // System.out.println(product);
-        }
-
-        // check if it contains X
-        System.out.println(products.contains(window));
-
-        products.remove(floorPanel);
-
-        System.out.println(products);
+        // For Map, List, Queque, just google it because the concepts are quite simple. Just look at the diagram on what to use, then from that interface
+        // You should later pick the IMPLEMENTATIONS that fits your need (some are faster at removal, etc).
+        // (y)
 
     }
 }
